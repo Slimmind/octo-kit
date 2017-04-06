2321ef41895b1c59d057d56a600ce4b0176e8db1
paul.s <paul.s@bigdropinc.com>
2017-04-06 10:19:01 +0300

    reinitial commit

---
+++ assets/js/home.js
@@ -0,0 +1,33 @@
+/* //= includes/plugin_name.js */
+
+(function ($, window, document) {
+  'use strict';
+  var page = {
+    init: function () {
+    },
+    noDev: function () {
+      if ((typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1)) {
+        $('.no-dev').removeClass('no-dev');
+      }
+    },
+    isDev: function () {
+      return !$('html').hasClass('no-dev') || window.innerWidth < 1280;
+    },
+    load: function () {
+    },
+    resize: function () {
+    },
+    scroll: function () {
+    }
+  };
+
+  $(document).ready(page.init);
+  $(window).on({
+    'load': page.load,
+    'resize': page.resize,
+    'scroll': page.scroll
+  });
+
+  window.isDevice = page.isDev;
+
+})(jQuery, window, document);
