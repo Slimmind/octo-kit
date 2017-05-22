ecdb60b2f5754f2d49d86768db95c99b51077bd0
paul.s <paul.s@bigdropinc.com>
2017-05-22 14:51:35 +0300

    replaced menu.html and pages.json with menu.php

---
+++ menu.php
@@ -0,0 +1,49 @@
+<?php
+    $dir = getcwd();
+    $f = scandir($dir);
+?>
+<style>
+    body {
+        background: url('bg.png');
+    }
+    ol {
+        width: 100%;
+        max-width: 480px;
+        margin: 30px auto;
+        padding: 0;
+    }
+    ol li {
+        display: block;
+        margin-bottom: 5px;
+    }
+    ol a {
+        display: block;
+        font-family: sans-serif;
+        font-size: 20px;
+        line-height: 24px;
+        border-radius: 5px;
+        border: 1px solid #aeaeae;
+        padding: 5px 15px;
+        width: auto;
+        color: #005689;
+        background-color: rgba(0, 100, 180, .3);
+        text-decoration: none;
+        transition-property: color, background-color;
+        transition: .3s linear;
+    }
+    ol a:hover {
+        background-color: rgba(0, 100, 180, .6);
+        color: #fff;
+    }
+        
+</style>
+
+<ol>
+    <?php foreach ($f as $file) :?>
+        <?php if (preg_match('/^(?!_).*html$/', $file)) :?>
+            <li style="margin-left: 20px;">
+                 <a href="<?php echo ($file)?>" target="_blank"><?php echo ($file)?></a>
+            </li>
+        <?php endif?>
+    <?php endforeach?>
+</ol>
